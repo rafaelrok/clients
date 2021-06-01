23b8b39d20366c1d22885e08894b99015be34532
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-05-31 22:05:02 -0300

    Finish project-clients

--- clients/src/main/java/com/rafaelvieira/clients/services/ClientService.java
+++ clients/src/main/java/com/rafaelvieira/clients/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.rafaelvieira.clients.services;
 
+import java.util.Optional;
+
 import com.rafaelvieira.clients.dto.ClientDTO;
 import com.rafaelvieira.clients.entities.Clients;
 import com.rafaelvieira.clients.repository.ClientRepository;
@@ -20,5 +22,40 @@ public class ClientService {
     public Page<ClientDTO> findAllPage(PageRequest pageRequest) {
         Page<Clients> poge = repository.findAll(pageRequest);
         return poge.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Clients> obj = repository.findById(id);
+        Clients entity = obj.get();
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO save(ClientDTO dto) {
+        Clients entity = new Clients();
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        Clients entity = repository.getOne(id);
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
     }
 }
