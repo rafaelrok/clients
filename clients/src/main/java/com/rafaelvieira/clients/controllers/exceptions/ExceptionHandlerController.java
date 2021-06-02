a1cf7e0f612e178f983b4c2efcc701e5a3b45823
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-06-01 21:59:28 -0300

    exception handling Clients

---
+++ clients/src/main/java/com/rafaelvieira/clients/controllers/exceptions/ExceptionHandlerController.java
@@ -0,0 +1,41 @@
+package com.rafaelvieira.clients.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.rafaelvieira.clients.services.handlers.DataBaseException;
+import com.rafaelvieira.clients.services.handlers.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandrError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandrError err = new StandrError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Não Encontrado");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandrError> dataBase(DataBaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandrError err = new StandrError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Database Exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- clients/src/main/java/com/rafaelvieira/clients/services/ClientService.java
+++ clients/src/main/java/com/rafaelvieira/clients/services/ClientService.java
@@ -2,10 +2,16 @@ package com.rafaelvieira.clients.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import com.rafaelvieira.clients.dto.ClientDTO;
 import com.rafaelvieira.clients.entities.Clients;
 import com.rafaelvieira.clients.repository.ClientRepository;
+import com.rafaelvieira.clients.services.handlers.DataBaseException;
+import com.rafaelvieira.clients.services.handlers.ResourceNotFoundException;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -27,7 +33,7 @@ public class ClientService {
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
         Optional<Clients> obj = repository.findById(id);
-        Clients entity = obj.get();
+        Clients entity = obj.orElseThrow(() -> new ResourceNotFoundException("Produto não encontrada"));
         return new ClientDTO(entity);
     }
 
@@ -45,17 +51,31 @@ public class ClientService {
 
     @Transactional
     public ClientDTO update(Long id, ClientDTO dto) {
-        Clients entity = repository.getOne(id);
-        entity.setName(dto.getName());
-        entity.setCpf(dto.getCpf());
-        entity.setIncome(dto.getIncome());
-        entity.setBirthDate(dto.getBirthDate());
-        entity.setChildren(dto.getChildren());
-        entity = repository.save(entity);
-        return new ClientDTO(entity);
+        try {
+            Clients entity = repository.getOne(id);
+            entity.setName(dto.getName());
+            entity.setCpf(dto.getCpf());
+            entity.setIncome(dto.getIncome());
+            entity.setBirthDate(dto.getBirthDate());
+            entity.setChildren(dto.getChildren());
+            entity = repository.save(entity);
+            return new ClientDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Categoria não encontrada " + id);
+        }
+        
     }
 
     public void delete(Long id) {
+    try {
         repository.deleteById(id);
+    } catch (EmptyResultDataAccessException ex) {
+        throw new ResourceNotFoundException("Categoria não encontrada " + id);
+        //TODO: handle exception
+    } catch (DataIntegrityViolationException err) {
+        throw new DataBaseException("Integrity Violation");
+    }
+
+        
     }
 }
